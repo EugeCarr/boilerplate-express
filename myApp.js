72f2d6a158e87c0f2c0f5eb2e0dffd599f1db4dd
EugeCarr <eugecarr@gmail.com>
2023-10-25 10:50:44 +0100

    fix; assets public

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.use('/public', express.static())
+app.use(express.static(__dirname + '/public'))
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + '/views/index.html')
