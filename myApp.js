3bd4eecc45717ea5c30c892a8393069eb3bcac61
EugeCarr <eugecarr@gmail.com>
2023-10-25 10:37:59 +0100

    test

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 
 const assets_path = __dirname + '/public';
 console.log(assets_path)
-app.use(__dirname + '/public', express.static())
+// app.use(__dirname + '/public', express.static())
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + '/views/index.html')
