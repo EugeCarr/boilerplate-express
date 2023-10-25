c0d9ba9777739ce0050daa652ba755af3c32539c
EugeCarr <eugecarr@gmail.com>
2023-10-25 10:57:47 +0100

    json response

--- myApp.js
+++ myApp.js
@@ -6,7 +6,12 @@ app.use('/public', express.static(__dirname + '/public'))
 app.get("/", function(req, res){
     res.sendFile(__dirname + '/views/index.html')
 })
-console.log("Hello World")
+
+app.get("/json", function(req, res){
+    res.json({
+        "message": "Hello json"
+    })
+})
 
 
 
