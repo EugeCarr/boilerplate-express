e3970962ecd813349c1eb391e833bcb2df021387
EugeCarr <eugecarr@gmail.com>
2023-10-25 10:47:01 +0100

    fix:path

--- myApp.js
+++ myApp.js
@@ -1,9 +1,7 @@
 let express = require('express');
 let app = express();
 
-const assets_path = __dirname + '/public';
-console.log(assets_path)
-app.use(assets_path, express.static())
+app.use('/public', express.static())
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + '/views/index.html')
