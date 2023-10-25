372857b4414c8d010b1a01735c3b6c69a7382fcf
EugeCarr <eugecarr@gmail.com>
2023-10-25 10:28:41 +0100

    added css file with use. task 4

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+app.use(__dirname + '/public', express.static())
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + '/views/index.html')
