3d835e386e84abe841af04e301f3266740a247ff
EugeCarr <eugecarr@gmail.com>
2023-10-25 10:37:20 +0100

    test commit for path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
+const assets_path = __dirname + '/public';
+console.log(assets_path)
 app.use(__dirname + '/public', express.static())
 
 app.get("/", function(req, res){
